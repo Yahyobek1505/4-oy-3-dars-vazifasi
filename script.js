30bbbe64fa40f7d11f65c1aba47f9fa70dace55c
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2023-12-12 15:39:44 +0500

    Homework is done.

---
+++ script.js
@@ -0,0 +1,62 @@
+/********************* Uyga vazifa masalalar *******************/
+// <=================>1-masala1<===========>.
+/* let num1 = 15;
+let num2 = 40;
+let num3 = 20;
+
+let sum = 0;
+let counter = 1;
+
+if (num1 % 2 == 0 && num1 % 5 == 0) {
+  sum += num1;
+  counter *= num1
+}
+if (num2 % 2 == 0 && num1 % 5 == 0)  {
+  sum += num2;
+  counter *= num2;
+}
+if (num3 % 2 == 0 && num1 % 5 == 0) {
+  sum += num3;
+  counter *= num3;
+}
+console.log(sum);
+ */
+
+// <==========================> 2-masala sharti. <==================>
+// let num1 = 50;
+// let num2 = 14;
+// let num3 = 10;
+// let num4 = 27;
+
+// let counter = 1;
+
+// if (num1 % 3 == 0 || num1 % 7 == 0) {
+//   counter *= num1;
+// }
+// if (num2 % 3 == 0 || num2 % 7 == 0) {
+//   counter *= num2;
+// }
+// if (num3 % 3 == 0 || num3 % 7 == 0) {
+//   counter *= num3;
+// }
+// if (num4 % 3 == 0 || num4 % 7 == 0) {
+//   counter *= num4;
+// }
+
+// console.log(counter);
+
+// <==========================> 3-masala sharti. <==================>
+
+// let num = prompt("Ikki xonali son kiriting" )
+
+// let left = num / 10;
+
+// let rightNumber = num % 10;
+// let leftNumber = Math.round(left);
+
+// let result = rightNumber * leftNumber;
+
+// alert(result);
+// console.log(result);
+// console.log(rightNumber);
+// console.log(leftNumber);
